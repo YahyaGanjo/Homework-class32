be5b450a02a50f47ff12725c6fa2019679989796
yahya ganjo <yahya.ganjo@gmail.com>
2021-05-11 01:57:45 +0200

    ex1 solved

--- 1-JavaScript/Week4/homework/ex1-doubleEvenNumbers.js
+++ 1-JavaScript/Week4/homework/ex1-doubleEvenNumbers.js
@@ -10,13 +10,8 @@ Let's rewrite it (or _refactor_ it, as professional developers would call it):
 ------------------------------------------------------------------------------*/
 function doubleEvenNumbers(numbers) {
   // TODO rewrite the function body using `map` and `filter`.
-  const newNumbers = [];
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 === 0) {
-      newNumbers.push(numbers[i] * 2);
-    }
-  }
-  return newNumbers;
+  const evenNumbers = numbers.filter((number) => number % 2 === 0);
+  return evenNumbers.map((number) => number * 2);
 }
 
 const myNumbers = [1, 2, 3, 4];
