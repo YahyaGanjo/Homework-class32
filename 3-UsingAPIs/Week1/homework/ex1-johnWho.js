41c51a00e31024a444f10779b950270b5c2c9cbf
yahya ganjo <yahya.ganjo@gmail.com>
2021-06-13 23:39:52 +0200

    ex1 solved

--- 3-UsingAPIs/Week1/homework/ex1-johnWho.js
+++ 3-UsingAPIs/Week1/homework/ex1-johnWho.js
@@ -7,20 +7,18 @@ Rewrite this function, but replace the callback syntax with the Promise syntax:
   didn't pass in a first name!"
 ------------------------------------------------------------------------------*/
 // TODO see above
-const getAnonName = (firstName, callback) => {
-  setTimeout(() => {
-    if (!firstName) {
-      callback(new Error("You didn't pass in a first name!"));
-      return;
+const getAnonName = (firstName) => {
+  return new Promise((resolve, reject) => {
+    if (firstName) {
+      const fullName = `${firstName} Doe`;
+      resolve(fullName);
+    } else {
+      reject(new Error("You didn't pass in a first name!"));
     }
-
-    const fullName = `${firstName} Doe`;
-
-    callback(fullName);
-  }, 1000);
+  });
 };
 
-getAnonName('John', console.log);
+getAnonName('John').then(console.log).catch(console.log);
 
 // ! Do not change or remove the code below
 module.exports = getAnonName;
